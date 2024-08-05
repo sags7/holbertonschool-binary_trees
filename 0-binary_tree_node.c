870c1123e5ba6e9dd288925efe2e89acf23940b2
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-08-05 12:05:08 -0500

    Feature: binary_tree_node() creates a node for a binary_tree_t

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - creates a node for a binary tree
+ * @parent: the parent of the node
+ * @value: the data stored in the node
+ * Return: a pointer to the node,or null on failure to create
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (!newNode)
+		return (NULL);
+	newNode->n = value;
+	newNode->parent = parent;
+
+	return (newNode);
+}
