494826f52cd235b5a40ccbebf8a1051880866357
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-08-06 09:56:53 -0500

    Feature: binary_tree_is_full() returns 1 if tree is full, 0 if not

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - returns one if tree is full
+ * @tree: the tree to check
+ * Return: 1 if full, 0 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int isFull = 1;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		isFull = binary_tree_is_full(tree->left);
+
+	if (tree->right)
+		isFull = binary_tree_is_full(tree->right);
+
+	if ((tree->left && !tree->right) || (!tree->left && tree->right))
+		isFull = 0;
+
+	return (isFull);
+}
